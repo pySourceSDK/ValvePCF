0953738ef61685852e09a05ec915e6d3d6ef021f
maxdup <mdupuis@hotmail.ca>
2021-02-14 07:38:43 -0500

    added default values where applicable

--- valvepcf/structs.py
+++ valvepcf/structs.py
@@ -10,25 +10,25 @@ standard_library.install_aliases()
 from construct import *
 
 attribute_types = {
-    1: Int32sl,  # ATTRIBUTE_ELEMENT
-    2: Int32sl,  # ATTRIBUTE_INTEGER
-    3: Float32l,   # ATTRIBUTE_FLOAT
-    4: Flag,  # ATTRIBUTE_BOOLEAN
-    5: Switch(lambda ctx: ctx._root.versions[0], {4: Int16ul, 5: Int32ul},
-              default=CString('ascii')),  # ATTRIBUTE_STRING
-    6: PrefixedArray(Int8ul, Byte),  # ATTRIBUTE_BINARY
-    7: Float32l,  # ATTRIBUTE_TIME
-    8: Int8ul[4],  # ATTRIBUTE_COLOR
-    9: Float32l[2],  # ATTRIBUTE_VECTOR2
-    10: Float32l[3],  # ATTRIBUTE_VECTOR3
-    11: Float32l[4],  # ATTRIBUTE_VECTOR4
-    12: Float32l[4],  # ATTRIBUTE_QANGLE
-    13: Float32l[4],  # ATTRIBUTE_QUATERNION
-    14: Float32l[4][4]  # ATTRIBUTE_MATRIX
+    1: Default(Int32sl, 0) * "ATTRIBUTE_ELEMENT",
+    2: Default(Int32sl, 0) * "ATTRIBUTE_INTEGER",
+    3: Default(Float32l, 0.0) * "ATTRIBUTE_FLOAT",
+    4: Default(Flag, False) * "ATTRIBUTE_BOOLEAN",
+    5: Default(Switch(lambda ctx: ctx._root.versions[0], {4: Int16ul, 5: Int32ul},
+                      default=CString('ascii')), None) * "ATTRIBUTE_STRING",
+    6: PrefixedArray(Int32ul, Byte) * "ATTRIBUTE_BINARY",
+    7: Default(Float32l, 0.0) * "ATTRIBUTE_TIME",
+    8: Default(Int8ul[4], [0, 0, 0, 255]) * "ATTRIBUTE_COLOR",
+    9: Default(Float32l[2], [0.0, 0.0]) * "ATTRIBUTE_VECTOR2",
+    10: Default(Float32l[3], [0.0, 0.0, 0.0]) * "ATTRIBUTE_VECTOR3",
+    11: Default(Float32l[4], [0.0, 0.0, 0.0, 0.0]) * "ATTRIBUTE_VECTOR4",
+    12: Default(Float32l[3], [0.0, 0.0, 0.0]) * "ATTRIBUTE_QANGLE",
+    13: Default(Float32l[4], [0.0, 0.0, 0.0, 1.0]) * "ATTRIBUTE_QUATERNION",
+    14: Float32l[4][4] * "ATTRIBUTE_MATRIX"
 }
 
 for n in range(1, 15):
-    attribute_types[n + 14] = PrefixedArray(Int32sl, attribute_types[n])
+    attribute_types[n + 14] = PrefixedArray(Int32ul, attribute_types[n])
 
 
 def build_versions(versions):
@@ -42,22 +42,22 @@ def parse_versions(vstring):
 CDmxAttribute = Struct(
     'typeNameIndex' / Switch(this._root.versions[0], {5: Int32ul},
                              default=Int16ul) * 'String dictionary index',
-    'attributeType' / Int8ul,
+    'attributeType' / Int8ul * "Determines the type of attributeData",
     'attributeData' / Switch(this.attributeType, attribute_types))
 
 CDmxElement = Struct(
     'typeNameIndex' / Int16ul,
     'elementName' / Switch(lambda ctx: ctx._root.versions[0], {4: Int16ul, 5: Int16ul},
                            default=CString('ascii')),
-    'dataSignature' / Switch(lambda ctx: ctx._root.versions[0], {5: Bytes(20)},
-                             default=Bytes(16)) * "Globally unique identifier")
+    'unknown' / If(lambda ctx: ctx._root.versions[0] == 5, Int32ul),
+    'dataSignature' / Default(Bytes(16), lambda ctx: uuid.uuid4().bytes) * "Globally unique identifier")
 
 PCF = Struct(
-    'version_string' / Rebuild(CString('ascii'),
-                               lambda ctx: build_versions(ctx.versions)),
-    'versions' / Computed(lambda ctx: parse_versions(ctx.version_string)),
+    'versionString' / Rebuild(CString('ascii'),
+                              lambda ctx: build_versions(ctx.versions)),
+    'versions' / Computed(lambda ctx: parse_versions(ctx.versionString)),
     'strings' / PrefixedArray(Switch(this._root.versions[0],
                                      {4: Int32ul, 5: Int32ul},
                                      default=Int16ul), CString('ascii')),
-    'elements' / PrefixedArray(Int32sl, CDmxElement),
-    'attributes' / PrefixedArray(Int32sl, CDmxAttribute)[len_(this.elements)])
+    'elements' / PrefixedArray(Int32ul, CDmxElement),
+    'attributes' / PrefixedArray(Int32ul, CDmxAttribute)[len_(this.elements)])
